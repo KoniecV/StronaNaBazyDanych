b9f23fee78ce063b0256f4c53e359bf111b1fbc0
Patryk Zatorski <115892476+Patryk-Zatorski@users.noreply.github.com>
2024-01-22 15:09:36 +0100

    push od kacpra ręcznie

--- notflask.py
+++ notflask.py
@@ -3,12 +3,17 @@ import logging
 import influxdb_client
 import pandas as pd
 from influxdb_client.client.write_api import SYNCHRONOUS
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash, Blueprint
 import psycopg2
 import json
 import requests
 from bs4 import BeautifulSoup
 from flask import send_file
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import send_file
+from flask_mail import Mail, Message
+from urllib.parse import quote
+import random 
 from urllib.parse import quote
 
 
@@ -39,20 +44,27 @@ def get_steam_market_image(skin_name):
 
 # Konfiguracja InfluxDB
 influxdb_host = "127.0.0.1:8086"
-bucket = "admin"
-org = "086f756ff907fbd5"
-token = "4khVY-qf0z-Bt3iGxam3IvXvExtahAwFRaflw_3hP1QNU38lFjpg-0ZQHlmb3FEU57wGQrZEu4Mvea-UFuXS8Q=="
+bucket = "Test"
+org = "Steam"
+token = "h3Pp4GlwPPbuJDVXkyaqEweBJE7TzvR3WJAJHd3Ft_zOiYmPZKs3h6GYG4EJvgpiXYQZRkH867mp5p63U_6nLg=="
 #org = "c9d2f82bec384031"
 #token = "YY7AtGmBB5uAAcgdEP5G0u34dqbbmEYmr7-ZgOEG4spK_6l9XMThk7HQckSQVWwD7mGxKSLzcTqHXU8bGU5pow=="
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234567'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'skindbmailer@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jpye ddvm zlgn jwtk'
+mail = Mail(app) 
 
 def connect_db():
     try:
         connection = psycopg2.connect(
             host="127.0.0.1",
             user="postgres",
-            password="admin",
+            password="rootpass",
             database="postgres",
             port="5432"
         )
@@ -62,6 +74,120 @@ def connect_db():
         print(f"Error connecting to the database: {e}")
         # Optionally, raise the exception to propagate it further
         raise
+
+
+def generate_random_code():
+    return str(random.randint(100000, 999999))
+
+send_email_bp = Blueprint('send_email_bp', __name__)
+
+@send_email_bp.route('/send_confirmation_email_from_profile', methods=['GET'])
+def send_confirmation_email_from_profile():
+    try:
+        # Dodaj kod do przekazania informacji o sesji i wywołania funkcji send_confirmation_email
+        if 'user' in session:
+            user_id = session['user']['id']
+            auth_code = generate_random_code()
+
+            # Przekazanie informacji o sesji do funkcji send_confirmation_email
+            send_confirmation_email(session, auth_code)
+
+            return redirect(url_for('profile'))
+        else:
+            return jsonify({'status': 'error', 'message': 'Użytkownik nie jest zalogowany'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': f'Błąd: {str(e)}'})
+
+# Dodaj Blueprint do aplikacji
+app.register_blueprint(send_email_bp)
+def send_confirmation_email(session, auth_code):
+    try:
+        if 'user' in session:
+            user_id = session['user']['id']
+            username = session['user']['username']
+
+            # Pobierz adres e-mail użytkownika z bazy danych
+            conn = connect_db()
+            cursor = conn.cursor()
+            cursor.execute("SELECT email FROM uzytkownik WHERE iduzytkownika = %s;", (user_id,))
+            user_email = cursor.fetchone()[0]
+            conn.close()
+
+            # Aktualizuj kod autoryzacyjny użytkownika w bazie danych
+            conn = connect_db()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE uzytkownik SET kodweryfikacyjny = %s WHERE iduzytkownika = %s;", (auth_code, user_id))
+            conn.commit()
+            conn.close()
+
+            # Wyślij wiadomość e-mail
+            msg = Message('Potwierdzenie rejestracji', sender='skindbmailer@gmail.com', recipients=[user_email])
+            msg.body = f'Twój kod to: {auth_code}'
+            mail.send(msg)
+            print('Wiadomość została wysłana')
+
+            # Dodanie informacji o sesji
+            session['confirmation_email_sent'] = True
+
+        else:
+            print('Brak zalogowanego użytkownika')
+
+    except Exception as e:
+        print(f'Błąd podczas wysyłania e-maila: {str(e)}')
+
+# Wywołanie funkcji send_confirmation_email
+@app.route('/send_confirmation_email')
+def send_confirmation_email_route():
+    try:
+        if 'user' in session:
+            user_id = session['user']['id']
+            auth_code = generate_random_code()
+
+            # Przekazanie informacji o sesji do funkcji send_confirmation_email
+            send_confirmation_email(session, auth_code)
+
+            return redirect(url_for('profile'))
+        else:
+            return 'Użytkownik nie jest zalogowany'
+    except Exception as e:
+        return f'Błąd: {str(e)}'
+
+@app.route('/confirm_registration', methods=['POST'])
+def confirm_registration():
+    try:
+        if 'user' in session:
+            user_id = session['user']['id']
+            confirmation_code = request.form.get('confirmation_code')
+
+            # Sprawdź, czy kod potwierdzenia jest poprawny
+            conn = connect_db()
+            cursor = conn.cursor()
+            cursor.execute("SELECT kodweryfikacyjny FROM uzytkownik WHERE iduzytkownika = %s;", (user_id,))
+            stored_confirmation_code = cursor.fetchone()[0]
+
+            conn.close()
+
+            if str(confirmation_code).strip() == str(stored_confirmation_code).strip():
+                # Zaktualizuj kolumnę 'zweryfikowany' w bazie danych
+                print(stored_confirmation_code)
+                print(confirmation_code)
+                conn = connect_db()
+                cursor = conn.cursor()
+                cursor.execute("UPDATE uzytkownik SET zweryfikowany = TRUE WHERE iduzytkownika = %s;", (user_id,))
+                conn.commit()
+                conn.close()
+
+                flash("Rejestracja została pomyślnie potwierdzona!", 'success')
+                return redirect(url_for('profile'))
+            else:
+                flash("Nieprawidłowy kod potwierdzenia.", 'error')
+                return redirect(url_for('profile')) \
+
+        else:
+            return redirect(url_for('login'))
+    except Exception as e:
+        flash(f"Błąd podczas potwierdzania rejestracji: {str(e)}", 'error')
+        return redirect(url_for('profile'))
 
 
 @app.route('/get_suggestions', methods=['GET'])
@@ -88,10 +214,6 @@ def fetch_suggestions_from_db(query):
     conn.close()
 
     return suggestions
-
-
-
-
 @app.route('/<skinName>')
 def skin(skinName):
     if skinName == "favicon.ico":
@@ -110,12 +232,82 @@ def skin(skinName):
     return render_template('skin.html', **context)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Check if the user exists in the database
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT iduzytkownika, nazwa, haslo FROM uzytkownik WHERE nazwa = %s;", (username,))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user and check_password_hash(user[2], password):
+            # Set session after logging in
+            session['user'] = {'id': user[0], 'username': user[1]}
+            return redirect(url_for('index'))
+        else:
+            flash("Błędna nazwa użytkownika lub hasło.", 'error')  # Dodaj ten flash message
+            #return "Błędna nazwa użytkownika lub hasło."
+
     return render_template('login.html')
 
-@app.route('/register')
-def register():
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        nazwa = request.form.get('username')
+        email = request.form.get('email')
+        steamid = request.form.get('steamid')
+        password = request.form.get('password')
+        password_confirm = request.form.get('password_confirm')
+
+        # Check if passwords match
+        if password != password_confirm:
+            flash("Hasła się nie zgadzają!", 'error')
+            return redirect(url_for('register_user'))
+
+        # Check if password length is at least 8 characters
+        if len(password) < 8:
+            flash("Hasło musi mieć conajmniej 8 znaków!", 'error')
+            return redirect(url_for('register_user'))
+
+        # Check if SteamID has exactly 17 characters
+        if len(steamid) != 17:
+            flash("SteamID musi mieć dokładnie 17 znaków!", 'error')
+            return redirect(url_for('register_user'))
+
+        # Check if username or email already exist
+        conn = connect_db()
+        cursor = conn.cursor()
+
+        try:
+            if user_exists(nazwa, email, cursor):
+                flash("Użytkownik o podanej nazwie lub emailu już istnieje!", 'error')
+                return redirect(url_for('register_user'))
+
+            # Hash the password before storing it
+            hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+
+            # Insert data into the database
+            cursor.execute("""
+                INSERT INTO uzytkownik (nazwa, email, haslo, steamid, zweryfikowany)
+                VALUES (%s, %s, %s, %s, %s);
+            """, (nazwa, email, hashed_password, steamid, False))
+
+            conn.commit()
+            return redirect(url_for('login'))
+
+        except Exception as e:
+            conn.rollback()
+            flash(f"Błąd podczas rejestracji: {e}", 'error')
+            return redirect(url_for('register_user'))
+
+        finally:
+            conn.close()
+
     return render_template('register.html')
 
 @app.route('/inventory')
@@ -136,11 +328,38 @@ def get_image_url(skin_name):
         return 'png/error.png' # Zmień na ścieżkę do domyślnego zdjęcia    
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    if 'user' in session:
+        user_id = session['user']['id']
+        username = session['user']['username']
+
+        # Pobierz dodatkowe informacje o użytkowniku z bazy danych
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT iduzytkownika, nazwa, email, zweryfikowany FROM uzytkownik WHERE iduzytkownika = %s;", (user_id,))
+        user_info = cursor.fetchone()
+        conn.close()
+
+        if user_info:
+            return render_template('profile.html', user_info=user_info)
+        else:
+            flash("Błąd pobierania informacji o użytkowniku", 'error')
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+# Wylogowanie
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
 @app.route('/query_influxdb', methods=['GET'])
 def query_influxdb():
     # Utwórz zapytanie Flux
     flux_query = '''
-    from(bucket: "admin")
+    from(bucket: "Test")
       |> range(start: 0, stop: now())
       |> last()
       |> filter(fn: (r) => r["Normal"] != "")
@@ -181,10 +400,10 @@ def get_data(weaponType,skinName):
     datax = []
     datay = []
     
-    for x in range(300, 0, -1):
+    for x in range(30, 0, -1):
         if x == 1:
             query = f'''
-            from(bucket: "admin")
+            from(bucket: "Test")
             |> range(start: -{x}d, stop: now())
             |> filter(fn: (r) => r["_measurement"] == "{weaponType}")
             |> filter(fn: (r) => r["Normal"] == "{skinName}")
@@ -192,7 +411,7 @@ def get_data(weaponType,skinName):
          '''
         else:
             query = f'''
-            from(bucket: "admin")
+            from(bucket: "Test")
             |> range(start: -{x}d, stop: -{x-1}d)
             |> filter(fn: (r) => r["_measurement"] == "{weaponType}")
             |> filter(fn: (r) => r["Normal"] == "{skinName}")
@@ -251,6 +470,61 @@ def skin_data_from_postgres(weaponType, skinName):
     conn.close()
     print(data)
     return data
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    if 'user' in session:
+        user_id = session['user']['id']
+        username = session['user']['username']
+
+        if request.method == 'POST':
+            current_password = request.form.get('current_password')
+            new_password = request.form.get('new_password')
+            confirm_new_password = request.form.get('confirm_new_password')
+
+            # Sprawdź, czy obecne hasło jest poprawne
+            conn = connect_db()
+            cursor = conn.cursor()
+            cursor.execute("SELECT haslo FROM uzytkownik WHERE iduzytkownika = %s;", (user_id,))
+            stored_password = cursor.fetchone()[0]
+            conn.close()
+
+            if not check_password_hash(stored_password, current_password):
+                flash("Obecne hasło jest nieprawidłowe.", 'error')
+                return redirect(url_for('change_password'))
+
+            if len(new_password) < 8:
+                flash("Hasło musi mieć conajmniej 8 znaków!", 'error')
+                return redirect(url_for('change_password'))
+
+            # Sprawdź, czy nowe hasła się zgadzają
+            if new_password != confirm_new_password:
+                flash("Nowe hasła się nie zgadzają.", 'error')
+                return redirect(url_for('change_password'))
+
+            # Zaktualizuj hasło w bazie danych
+            hashed_new_password = generate_password_hash(new_password, method='pbkdf2:sha256')
+            conn = connect_db()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE uzytkownik SET haslo = %s WHERE iduzytkownika = %s;", (hashed_new_password, user_id))
+            conn.commit()
+            conn.close()
+
+            flash("Hasło zostało pomyślnie zmienione.", 'success')
+            return render_template('change_password.html')
+
+        return render_template('change_password.html', username=username)
+    else:
+        return redirect(url_for('login'))
+def user_exists(username, email, cursor):
+    # Check if username or email already exist in the database
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM uzytkownik
+        WHERE nazwa = %s OR email = %s;
+    """, (username, email))
+
+    return cursor.fetchone()[0] > 0
 
 if __name__ == '__main__':
     #db.create_all()
